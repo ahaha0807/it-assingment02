91e5b758bb84e43da951efc32bfe273c1639e135
ahaha0807 <b5331@oic.jp>
2017-07-19 09:32:12 +0900

    Add task destroy
    [#24]
    - タスクを削除するコントローラーの追加
    - 同時にビューの更新

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,12 @@ class TasksController < ApplicationController
         @task = @project.tasks.create(task_params)
         redirect_to project_path(@project.id)
     end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to project_path(params[:project_id])
+    end
 
     private
         def task_params
